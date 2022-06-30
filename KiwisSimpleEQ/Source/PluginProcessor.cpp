b21d7a058fe1918b8175d38e1fe073394039d7ca
keeweefruchtig <jan.dominiak@web.de>
2022-06-30 23:11:30 +0200

    Added PluginHost, Parameters, DSP

--- KiwisSimpleEQ/Source/PluginProcessor.cpp
+++ KiwisSimpleEQ/Source/PluginProcessor.cpp
@@ -155,18 +155,16 @@ void KiwisSimpleEQAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer (channel);
-
-        // ..do something to the data...
-    }
+    juce::dsp::AudioBlock<float> block(buffer);
+    
+    auto LeftBlock = block.getSingleChannelBlock(0);
+    auto RightBlock = block.getSingleChannelBlock(1);
+    
+    juce::dsp::ProcessContextReplacing<float> leftContext(LeftBlock);
+    juce::dsp::ProcessContextReplacing<float> rightContext(RightBlock);
+    
+    leftChain.process(leftContext);
+    rightChain.process(rightContext);
 }
 
 //==============================================================================
